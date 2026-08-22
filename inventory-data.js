27cdfb4b80d38f02b9bb861933c627d34adf62b2
JMOinsurance <dawsonjakoubek3@gmail.com>
2026-08-22 14:33:06 -0500

    Auto-sync: update showroom stock from ops app

--- inventory-data.js
+++ inventory-data.js
@@ -56,9 +56,9 @@ window.LIVE_INVENTORY = {
   "140": {
     "black": {
       "left": 1,
-      "right": 0,
-      "middle": 0,
-      "ottoman": 1,
+      "right": 1,
+      "middle": 1,
+      "ottoman": 2,
       "wedge": 0
     },
     "grey": {
@@ -69,11 +69,11 @@ window.LIVE_INVENTORY = {
       "wedge": 0
     },
     "white": {
-      "left": 0,
-      "right": 0,
-      "middle": 1,
-      "ottoman": 0,
-      "wedge": 0
+      "left": 1,
+      "right": 1,
+      "middle": 2,
+      "ottoman": 1,
+      "wedge": 1
     },
     "khaki": {
       "left": 0,
@@ -248,4 +248,4 @@ window.LIVE_CONFIG_REQUIREMENTS = {
     }
   }
 };
-window.LIVE_INVENTORY_SYNCED_AT = "2026-08-22T19:11:38.855Z";
+window.LIVE_INVENTORY_SYNCED_AT = "2026-08-22T19:33:05.596Z";
